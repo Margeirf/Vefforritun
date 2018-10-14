de05bd6e0fb3994a4fdbe4065d9972d0d4f325be
Margeir <margeirf@gmail.com>
2018-10-14 17:36:07 +0000

    Delete main.js

--- VefMidannarverk/main.js
+++
@@ -1,9 +0,0 @@
-from bottle import run, route, static_file, error, template
-
-@route ('/')
-def index():
-    with urllib.request.urlopen("http://apis.is/currency/m5") as url:
-        data = json.loads.(url.read().decode())
-    return data
-
-run(host='localhost', port=8080, debug=True, reloader=True)
